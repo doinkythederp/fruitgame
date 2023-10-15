a2ba27573b5fed68ba4601c5e4f742970ff605e1
doinkythederp <doinkythederp@icloud.com>
2023-10-15 06:33:15 +0000

    feat: pwa support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,29 @@
 import { defineConfig } from "astro/config";
 
 import preact from "@astrojs/preact";
+import { VitePWA } from "vite-plugin-pwa";
+import { manifest } from "./utils/pwaConfig";
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [preact()],
     site: "https://doinkythederp.github.io",
     base: "/fruitgame",
+    vite: {
+        plugins: [
+            VitePWA({
+                registerType: "autoUpdate",
+                manifest,
+                workbox: {
+                    globDirectory: "dist",
+                    globPatterns: [
+                        "**/*.{html,js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}",
+                    ],
+                    // Don't fallback on document based (e.g. `/some-page`) requests
+                    // This removes an errant console.log message from showing up.
+                    navigateFallback: null,
+                },
+            }),
+        ],
+    },
 });
---
+++ utils/pwaConfig.ts
@@ -0,0 +1,47 @@
+// Type imports
+import type { ManifestOptions } from "vite-plugin-pwa";
+
+/**
+ * Defines the default SEO configuration for the website.
+ */
+export const seoConfig = {
+    baseURL: "https://doinkythederp.github.com/fruitgame", // Change this to your production URL.
+    description: "fruit game lets you pop fruits. look for large groups!", // Change this to be your website's description.
+    type: "website",
+    image: {
+        url: "/fruitgame/android-chrome-512x512.png", // Change this to your website's thumbnail.
+        alt: "Watermelon icon", // Change this to your website's thumbnail description.
+        width: 512,
+        height: 512,
+    },
+    siteName: "fruit game", // Change this to your website's name,
+    twitter: {
+        card: "summary_large_image",
+    } as const,
+};
+
+/**
+ * Defines the configuration for PWA webmanifest.
+ */
+export const manifest: Partial<ManifestOptions> = {
+    name: seoConfig.siteName, // Change this to your website's name.
+    short_name: seoConfig.siteName, // Change this to your website's short name.
+    description: seoConfig.description, // Change this to your websites description.
+    theme_color: "#e53c40", // Change this to your primary color.
+    background_color: "#f5f5f5", // Change this to your background color.
+    display: "standalone",
+    icons: [
+        {
+            src: "/fruitgame/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+        },
+        {
+            src: "/fruitgame/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+        },
+    ],
+    scope: "/fruitgame/",
+    start_url: "/fruitgame",
+};
