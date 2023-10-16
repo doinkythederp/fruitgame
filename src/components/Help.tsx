5dc0dad1044089fb823ea26c114845c1396fb6cd
doinkythederp <doinkythederp@icloud.com>
2023-10-15 20:59:25 -0700

    feat: add help bar

---
+++ src/components/Help.tsx
@@ -0,0 +1,35 @@
+import { useState } from "preact/hooks";
+import "./help.css";
+
+export default function Help() {
+    const [helpOpen, setHelpOpen] = useState(false);
+    return (
+        <>
+            <button
+                class="toggle-help"
+                type="button"
+                onClick={() => setHelpOpen((open) => !open)}
+                title="Help"
+            >
+                ?
+            </button>
+            <div class="overlay">
+                <div class="sidebar" data-open={helpOpen}>
+                    <h2>Instructions</h2>
+                    <div>
+                        <h3>Goal</h3>
+                        <p>
+                            Pop fruit in groups of 3 or more to score points.
+                            Groups can be connected horizontally, vertically, or
+                            diagonally.
+                        </p>
+                    </div>
+                    <div>
+                        <h3>Points</h3>
+                        <p>Some fruit are worth more points than others.</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
